7a030eea0a6095259fbc55c856490755208e706b
Bazooka J <grubcowboy@gmail.com>
2025-07-20 23:34:56 -0700

    takes user city input instead of hard-coded lat long. call geoAPI to find lat long for input. get weather and change UI to display weather. units changed to US customary units

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,10 +1,21 @@
-let lat = 37.87279824815945;
-let long = -122.27535735891323;
-console.log(lat, long);
+const searchBox = document.querySelector('.search-box');
+const cityInput = document.querySelector('#city-input');
 
-(async function fetchWeather() {
-    const apiUrl = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&current_weather=true`;
-    console.log(apiUrl);
+
+searchBox.addEventListener('click', () => {
+    getLatLong(cityInput.value);
+    cityInput.value = '';
+});
+
+searchBox.addEventListener('keydown', e => {
+    if (e.key === 'Enter') {
+        getLatLong(cityInput.value);
+        cityInput.value = '';
+    }
+});
+
+async function fetchWeather(name, lat, long) {
+    const apiUrl = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&current=wind_speed_10m,temperature_2m,relative_humidity_2m&timezone=auto&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch`;
     try {
         const response = await fetch(apiUrl);
         if (!response.ok) {
@@ -13,8 +24,43 @@ console.log(lat, long);
 
         const json = await response.json();
         console.log(json);
+
+        const cityName = document.querySelector('#city-name');
+        cityName.innerHTML = `${name}`;
+
+        const temp = document.querySelector('#temperature');
+        temp.innerHTML = `${json.current.temperature_2m}℉`;
+
+        const humidity = document.querySelector('#humidity');
+        humidity.innerHTML = `Humidity: ${json.current.relative_humidity_2m}%`;
+
+        const wind = document.querySelector('#wind');
+        wind.innerHTML = `Wind: ${json.current.wind_speed_10m}mph`;
+
+        const weatherCard = document.querySelector('.weather-card');
+        weatherCard.classList.remove('hidden');
+
     } catch (error) {
         console.error(error.message);
     }
 
-})();
+};
+
+async function getLatLong(city) {
+    const geoUrl = `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1&language=en&format=json`;
+    try {
+        const response = await fetch(geoUrl);
+
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        console.log(json.results[0]);
+        fetchWeather(json.results[0].name, json.results[0].latitude, json.results[0].longitude);
+
+
+    } catch (error) {
+        console.error(error.message);
+    }
+};
